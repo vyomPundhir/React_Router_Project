e97a208435e07925520b3c9516596db4b7eaadcc
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-10-17 21:53:56 +0530

    Main jsx file added.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,66 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import User from './components/User/User'
+import Github, { githubInfoLoader } from './components/Github/Github'
+
+// This is one is the first method of routing
+
+// const router = createBrowserRouter(
+//   [
+
+//     {
+//       path: '/',
+
+//       element: <Layout/>,
+
+//       children:[
+//         {
+//           path:"",
+//           element:<Home/>
+//         },
+
+//         {
+//           path:"about",
+//           element: <About />
+//         },
+
+//         {
+//           path:"contact",
+//           element: <Contact />
+//         }
+
+//       ]
+//     }
+//   ]
+// )
+
+// This is the second method of routing given below:
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route
+        loader={githubInfoLoader}
+        path='github' 
+        element={<Github/>}
+      />
+    </Route>
+  )
+)
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
